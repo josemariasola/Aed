2d1a8e4653c14c00c2a20c992b0a0a90aaba8e5a
Yamil2017 <pechyar@hotmail.com>
2018-11-10 14:44:55 -0300

    Paso de definiciones al header

--- TpOpcionalTemplates/Template.h
+++ TpOpcionalTemplates/Template.h
@@ -2,24 +2,54 @@
 #define TEMPLATE_H
 
 template<typename T>
-class Matriz {
+struct Matriz {
 	int nFilas,nColumnas;
 	T*matriz;
 	
-public:
 	Matriz();
 	Matriz(int);
 	Matriz(int, int);
+	~Matriz();
 };
 
 template<typename T>
-Matriz<T> set(Matriz<T>&,T,int,int);
+Matriz<T>::~Matriz() {
+	delete[] matriz;
+}
 
 template<typename T>
-void print(const Matriz<T>&);
+Matriz<T>::Matriz() : nFilas{ 2 }, nColumnas{ 2 }, matriz{ nullptr }{
+	T*matriz = new T(nColumnas*nFilas);
+}
 
 template<typename T>
-T get(const Matriz<T>&, int, int);
+Matriz<T>::Matriz(int a) : nFilas{ a }, nColumnas{ a }, matriz{ nullptr }{
+	T*matriz = new T(nColumnas*nFilas);
+}
 
+template<typename T>
+Matriz<T>::Matriz(int fila, int columna) : nFilas{ fila }, nColumnas{ columna }, matriz{ nullptr }{
+	T*matriz = new T(nColumnas*nFilas);
+}
+
+template<typename T>
+Matriz<T>& set(Matriz<T>& matrix, T valor, int fila, int columna) {
+	short int elemento = (fila - 1)*matrix.nColumnas + columna - 1;
+	matrix.matriz[elemento] = valor;
+	return matrix;
+}
+
+template<typename T>
+void print(const Matriz<T>& matrix) {
+	for (int i=0;i<matrix.nColumnas*matrix.nFilas;i++)
+		std::cout << matrix.matriz[i] << " ";
+	std::cout << "\nFin de la impresion." << std::endl;
+}
+
+template<typename T>
+T& get(const Matriz<T>& matrix, int fila, int columna) {
+	short int elemento = (fila - 1)*matrix.nColumnas + columna - 1;
+	return matrix.matriz[elemento];
+}
 
 #endif
